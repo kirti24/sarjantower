275f5d3b98412e46b5cf3bc9891c7e0f2f6fdcb3
kirti24 <kirti.agrwl24@gmail.com>
2014-08-25 21:03:56 +0530

    test

--- src/main/java/com/example/HelloServlet.java
+++ src/main/java/com/example/HelloServlet.java
@@ -17,7 +17,7 @@ public class HelloServlet extends HttpServlet {
             throws ServletException, IOException {
         ServletOutputStream out = resp.getOutputStream();
         
-        out.write("Hello Heroku".getBytes());
+        out.write("Hello Heroku Version 1".getBytes());
         out.flush();
         out.close();
     }
