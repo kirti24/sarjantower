ce2f013907bf026d3260330f58ef34e2c4067572
kirti24 <kirti.agrwl24@gmail.com>
2014-08-25 21:09:22 +0530

    db test

--- src/main/java/com/example/HelloServlet.java
+++ src/main/java/com/example/HelloServlet.java
@@ -1,6 +1,7 @@
 package com.example;
 
 import java.io.IOException;
+import java.net.URI;
 import java.util.logging.Logger;
 
 import javax.servlet.ServletConfig;
@@ -16,7 +17,20 @@ public class HelloServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
         ServletOutputStream out = resp.getOutputStream();
+        URI dbUri = null;
+        try{
+	        dbUri = new URI(System.getenv("DATABASE_URL"));
+	
+	        String username = dbUri.getUserInfo().split(":")[0];
+	        String password = dbUri.getUserInfo().split(":")[1];
+	        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
+        }
+        catch (Exception e){
+        	e.printStackTrace();
+        }
         
+        String msg = dbUri.getHost() + "|" + dbUri.getPath()+"|"+dbUri.getPort();
+        out.write(msg.getBytes());
         out.write("Hello Heroku Version 1".getBytes());
         out.flush();
         out.close();
