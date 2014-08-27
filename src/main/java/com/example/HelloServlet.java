e1a67d360d801187f7abcaf3ef9e01b0bd14d64a
kirti24 <kirti.agrwl24@gmail.com>
2014-08-27 22:26:18 +0530

    test

--- src/main/java/com/example/HelloServlet.java
+++ src/main/java/com/example/HelloServlet.java
@@ -2,6 +2,9 @@ package com.example;
 
 import java.io.IOException;
 import java.net.URI;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
 import java.util.logging.Logger;
 
 import javax.servlet.ServletConfig;
@@ -19,7 +22,39 @@ public class HelloServlet extends HttpServlet {
             throws ServletException, IOException {
         ServletOutputStream out = resp.getOutputStream();
         
-        out.write((DBConnect.getConnection()).getBytes());
+        
+        URI dbUri = null;
+		Connection conn = null;
+	    try{
+	        dbUri = new URI(System.getenv("DATABASE_URL"));
+	        String username = dbUri.getUserInfo().split(":")[0];
+	        String password = dbUri.getUserInfo().split(":")[1];
+	        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
+	        conn = DriverManager.getConnection(dbUrl, username, password);
+	        Statement stat = conn.createStatement();
+	        out.write((conn.isClosed()+"").getBytes());
+	        /*ResultSet rs = stat.executeQuery("select count(*) from accounts");
+	        if(rs.next()){
+	        	return rs.getString(1);
+	        }*/
+	    }
+	    catch (Exception e){
+	    	e.printStackTrace();
+	    	//return null;
+	    }
+	    finally{
+	    	try{
+	    		if(conn!=null){
+	    			conn.close();
+	    		}
+	    	}
+	    	catch(Exception e){
+	    		e.printStackTrace();
+	    	}
+	    }
+        
+        
+        //out.write((DBConnect.getConnection()).getBytes());
         //out.write("Hello Heroku Version 1".getBytes());
         out.flush();
         out.close();
