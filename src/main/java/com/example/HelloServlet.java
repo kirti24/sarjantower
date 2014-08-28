ed3fa9926b55a1c01985c12be2843278089c5c4f
kirti24 <kirti.agrwl24@gmail.com>
2014-08-29 00:07:54 +0530

    Dispatcher

--- src/main/java/com/example/HelloServlet.java
+++ src/main/java/com/example/HelloServlet.java
@@ -3,11 +3,15 @@ package com.example;
 import java.io.IOException;
 
 
+
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 public class HelloServlet extends HttpServlet {
 
@@ -21,6 +25,23 @@ public class HelloServlet extends HttpServlet {
 		String password = req.getParameter("password");
 		out.write((username+password).getBytes());
 		boolean isValid = DBConnect.loginCheck(username,password);
+		
+		if(!isValid){
+			RequestDispatcher dispatcher = req.getRequestDispatcher("/pages/login.jsp");
+			req.setAttribute("errormessage", "Invalid Username/Password. Try again.");
+			req.getSession(false).invalidate();
+			if(dispatcher != null) dispatcher.forward(req, resp);
+		}else{
+			//DBConnect.sessionCreate(username, req.getSession(false).getId());
+			RequestDispatcher dispatcher = req.getRequestDispatcher("/index1.jsp");
+			req.setAttribute("verified", "true");						
+			HttpSession session = req.getSession(false);			
+			session.setAttribute("isAuthorised", "true");
+			session.setAttribute("user", username.toUpperCase());
+			System.out.println("Setting attribute isauth for session " + session.getId());
+			if(dispatcher != null) dispatcher.forward(req, resp);
+			return;
+		}
 
 	}
 
