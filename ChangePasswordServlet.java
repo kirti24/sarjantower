3502f8781608fa94cf81585d9b55e96bb083e3c2
kirti24 <kirti.agrwl24@gmail.com>
2015-09-13 19:31:30 +0530

    test

---
+++ ChangePasswordServlet.java
@@ -0,0 +1,63 @@
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+
+public class ChangePasswordServlet extends HttpServlet {
+	public ChangePasswordServlet() {
+        super();
+    }
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		/*
+		try{
+			ServletOutputStream out = resp.getOutputStream();
+			
+			String password = req.getParameter("password");
+			String repassword = req.getParameter("repassword");
+			out.write((password+"\t"+repassword).getBytes());
+			
+			if(!password.equals(repassword)){
+				RequestDispatcher dispatcher = req.getRequestDispatcher("/dashboard.jsp");
+				req.setAttribute("errormessage", "Password Mismatch. Try again.");
+				req.getSession(false).invalidate();
+				if(dispatcher != null) dispatcher.forward(req, resp);
+			}else{
+				//DBConnect.sessionCreate(username, req.getSession(false).getId());
+				RequestDispatcher dispatcher = req.getRequestDispatcher("/dashboard.jsp");
+				req.setAttribute("verified", "true");
+				req.setAttribute("user", username);
+				HttpSession session = req.getSession(false);			
+				session.setAttribute("isAuthorised", "true");
+				session.setAttribute("user", username);
+				if (!isValidated)
+					session.setAttribute("validated", "false");
+				else 
+					session.setAttribute("validated", "true");
+				
+				System.out.println("Setting attribute isauth for session " + session.getId());
+				if(dispatcher != null) dispatcher.forward(req, resp);
+				return;
+			}
+		}catch(Exception e){
+			RequestDispatcher dispatcher = req.getRequestDispatcher("/error.jsp");
+			if(dispatcher != null) dispatcher.forward(req, resp);
+			return;
+		}*/
+
+	}
+
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		doPost(req, resp);		
+	}
+
+}
--- src/main/webapp/changepwd.jsp
+++ src/main/webapp/changepwd.jsp
@@ -1,11 +1,25 @@
 <div class="row">
+	<div class="col-md-4"></div>
 	<div class="col-md-4">
-		<form action="changepwd" method="post">
+		<form action="changepwd" method="post" id="changepwdform" onsubmit="check_values();">
 			<input id="password" class="form-control login-field" name="password"
 				type="password" placeholder="Password"><br /> <input
 				id="repassword" class="form-control login-field" name="repassword"
 				type="password" placeholder="Re-Type password"><br />
-			<input type="submit" class="btn btn-primary btn-lg btn-block">
+			<button class="btn btn-primary btn-lg btn-block">Submit</button>
 		</form>
+		<div class=""></div>
 	</div>
 </div>
+
+<script type="text/javascript">
+function check_values() {
+    if ($("#repassword").val().length == 0 || $("#password").val().length == 0) {
+        $('password').addClass('has-error');
+        $('repassword').addClass('has-error');
+    }
+    else {
+    	$("#changepwdform").submit();
+    }
+}
+</script>
